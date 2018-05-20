38f2f2cd9364b3fde6b0792c22e5f98f9cc6d6d1
Marco Mark <marco@silvanite.com>
2018-05-20 21:00:03 +0100

    Import default Tailwind via Sass

--- src/stubs/webpack.mix.js
+++ src/stubs/webpack.mix.js
@@ -4,6 +4,6 @@ require('laravel-mix-tailwind')
 require('laravel-mix-purgecss')
 
 mix.js('resources/assets/js/app.js', 'js')
-    .sass('resources/assets/sass/app.scss', 'css')
+    .sass('resources/assets/sass/app.sass', 'css')
     .tailwind()
     .purgeCss()
